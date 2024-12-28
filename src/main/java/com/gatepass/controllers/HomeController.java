c549b8793b86aa91e052ad3913f1e3b38950cf41
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-12-28 13:40:59 +0530

    All the files are set-up and ready to start

---
+++ src/main/java/com/gatepass/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.gatepass.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
+
+public class HomeController {
+    @GetMapping()
+    public String index(){
+        return "This is the new era of Learning and growing!";
+    }
+}
