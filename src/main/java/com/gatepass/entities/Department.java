fc5509cf5504c95cfc945ad941b73a4449802cd1
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-01-04 00:23:00 +0530

    database connection has been established, resolved dependency problem also

---
+++ src/main/java/com/gatepass/entities/Department.java
@@ -0,0 +1,31 @@
+package com.gatepass.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Table(name = "department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long dptid;
+
+    @Column(nullable = false, length = 60)
+    private String deptname;
+
+    @Column()
+    private String address;
+
+    @Column
+    private String dept_Description;
+
+}
