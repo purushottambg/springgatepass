2df7307a162621cb2e62d2a7ec262e2cae31227d
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-01-05 23:56:56 +0530

    Name Amguity for the form entity has been fixed, which was causing trouble

--- src/main/java/com/gatepass/controllers/OpsController.java
+++ src/main/java/com/gatepass/controllers/OpsController.java
@@ -1,6 +1,9 @@
 package com.gatepass.controllers;
 
+import com.gatepass.entities.MembershipRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OpsController {
 
     @PostMapping("ops/save-request")
-    public String saveRequestData(){            //Data entered by the user must be saved
-        return "ops/save-request";
+    public String saveRequestData(Model model, @ModelAttribute("membershipRequest") MembershipRequest membershipRequest){            //Data entered by the user must be saved
+        MembershipRequest membershipRequest1=new MembershipRequest();
+        model.addAttribute("membershipRequest1", membershipRequest1);
+        return "ops/saved-request";
     }
 }
