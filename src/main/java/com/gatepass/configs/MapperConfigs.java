ecd3a72d0ba5fa6a2be3d0184bc1ce790a206ddd
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-01-09 23:59:11 +0530

    Model Mapper dependency was missing in pom.xml file. added with this commit

--- src/main/java/com/gatepass/configs/MapperConfigs.java
+++ src/main/java/com/gatepass/configs/MapperConfigs.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 
 public class MapperConfigs {
 
+    // Model mapper is a library that can be used to map one object to another
+    // For example here we need to map an Entity to the DTO
+
     @Bean
     public ModelMapper getModelMapper(){
         return new ModelMapper;
