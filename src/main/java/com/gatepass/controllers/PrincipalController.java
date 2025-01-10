f5d9572b98e7c568804d0324af1acb8920c833ae
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-01-10 10:04:25 +0530

    All the home pages are included, now the functionalities turn

---
+++ src/main/java/com/gatepass/controllers/PrincipalController.java
@@ -0,0 +1,15 @@
+package com.gatepass.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PrincipalController {
+
+    @GetMapping("pages/principal")
+    public String hodHome(){
+        return "pages/principal";
+    }
+}
