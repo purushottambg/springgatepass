fdb77ec09d04b602a12b4d73aface38bda228eed
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-01-14 19:59:01 +0530

    All the data for staff and department has been inserted

---
+++ src/main/java/com/gatepass/gatepassDB.sql
@@ -0,0 +1,18 @@
+-- This data is for the gate pass application which might need to be used again till
+-- the time project is in dev environment. Handle with Care!
+
+
+INSERT INTO `department` (`dptid`, `deptname`, `address`, `description`) VALUES
+(0, 'this is else block', NULL, NULL),
+(1, 'MCA Department', 'Main Building 3rd floor north wing', ''),
+(2, 'MBA Department', 'Main Building 3rd floor south wing', ''),
+(3, 'Computer Engineering', ' ', ''),
+(4, 'E&TC  Engineering', ' ', ''),
+(5, 'Electrics  Engineering', ' ', ''),
+(6, 'IT Engineering', ' ', ''),
+(7, 'Mechanical Engineering', ' ', ''),
+(8, 'FE Engineering', ' ', ''),
+(9, 'Library', ' ', ''),
+(10, 'Office Department', 'Main Building second floor', 'This is created for those who does not fall under any category');
+
+
