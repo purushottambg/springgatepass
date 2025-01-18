3acde24bc99969bab039161ebcb2826995e42963
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-01-18 18:05:58 +0530

    Cleark Entity having naming conventions issue

--- src/main/java/com/gatepass/repository/ClerkRepo.java
+++ src/main/java/com/gatepass/repository/ClerkRepo.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface ClerkRepo extends JpaRepository<ClerkEntity, Long> {
-    public Optional<ClerkEntity> findByUsername(String userName);
+    public Optional<ClerkEntity> findByUserName(String UserName);
 }
