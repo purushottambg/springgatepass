70e488deb249be191b17692f01b538a48ae134e0
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-01-22 10:00:42 +0530

    Started with the Auditing

---
+++ src/main/java/com/gatepass/models/AuditableEntity.java
@@ -0,0 +1,33 @@
+package com.gatepass.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EnableJpaAuditing
+@EntityListeners(EntityListeners.class)
+@Getter
+@Setter
+public class AuditableEntity{
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    private String updatedBy;
+}
