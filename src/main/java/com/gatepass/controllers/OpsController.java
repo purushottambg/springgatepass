3fcc524c80d3b76361c1516a608e143923c2776f
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-01-23 11:31:10 +0530

    git logging is enabled

--- src/main/java/com/gatepass/controllers/OpsController.java
+++ src/main/java/com/gatepass/controllers/OpsController.java
@@ -2,6 +2,8 @@ package com.gatepass.controllers;
 
 import com.gatepass.dtos.LoginDTO;
 import com.gatepass.service.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class OpsController {
-
+    Logger logger = LoggerFactory.getLogger(OpsController.class);
     @Autowired
     private MembershipRequestService membershipRequestService;
     @Autowired
@@ -32,26 +34,32 @@ public class OpsController {
 
         if(membershipRequestService.existByUserName(loginDTO.getUserName())) {
             model.addAttribute("response", "Hi, " + loginDTO.getUserName() + " your membership is not approved yet");
+            logger.info("Found user into the membership requested");
+            logger.debug("This should be redirected to the members");
+            logger.error("This is the error in case");
+            logger.trace("This is the trace in case");
+
             return "pages/member-request";               //Redirection should be based on the user type
         } else if (staffService.existByUserName(loginDTO.getUserName())) {
             model.addAttribute("response", "Hi, " + loginDTO.getUserName() + " Welcome");
             model.addAttribute("success", loginDTO);
+            logger.info("user found in the staff records");
             return "pages/staff";
         } else if (hodService.existByUserName(loginDTO.getUserName())) {
             model.addAttribute("response","Hi, " + loginDTO.getUserName() + " Welcome");
             model.addAttribute("success", loginDTO);
+            logger.info("user is the HOD");
             return "pages/hod-home";
         } else if (clerkService.existByUserName(loginDTO.getUserName())) {
             model.addAttribute("response","Hi, " + loginDTO.getUserName() + " Welcome");
             model.addAttribute("success", loginDTO);
+            logger.debug("User is the Clerk in college");
             return "pages/clerk";
         } else if (principalService.existByUserName(loginDTO.getUserName())) {
             model.addAttribute("response","Hi, " + loginDTO.getUserName() + " Welcome");
             model.addAttribute("success", loginDTO);
+            logger.info("User is the Principal");
             return "pages/principal";
-        } else if (bindingResult.hasErrors()) {
-            model.addAttribute("failureResponse", "it has some errors");
-            return "index";
         } else {
             model.addAttribute("failureResponse", "Did not find");
             return "index";
