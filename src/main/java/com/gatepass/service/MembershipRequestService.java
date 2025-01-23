9db5ce2d39b98f1b2edb799adb1c6ccea65ae732
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-01-23 11:31:10 +0530

    git logging is enabled

--- src/main/java/com/gatepass/service/MembershipRequestService.java
+++ src/main/java/com/gatepass/service/MembershipRequestService.java
@@ -2,6 +2,8 @@ package com.gatepass.service;
 
 import com.gatepass.models.MembershipEntity;
 import com.gatepass.repository.MembershipRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +12,13 @@ public class MembershipRequestService {
 
     @Autowired
     private MembershipRepo membershipRepo;
-
+    Logger logger = LoggerFactory.getLogger(MembershipRequestService.class);
     public MembershipEntity saveRequest(MembershipEntity membershipEntity){
         String firstName = membershipEntity.getFname().toLowerCase();  //Turn the program to the lower case as a good practice
         String phone = membershipEntity.getPhone();
         String userName = firstName+phone.substring(phone.length()-4);
+        logger.info("Username converted to the LowerCase!");
+        logger.trace("This is trace, original userID {}, converted to {}", membershipEntity.getFname()+phone.substring(phone.length()-4), userName);
         membershipEntity.setUsername(userName);   //Set Username for the new member
         return membershipRepo.save(membershipEntity);
     }
