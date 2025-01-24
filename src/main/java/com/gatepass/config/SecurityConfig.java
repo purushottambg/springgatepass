bf6e250c4b13b421a4a365f623c80d18d0130ecd
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-01-25 01:09:27 +0530

    Basic all the features are added

--- src/main/java/com/gatepass/config/SecurityConfig.java
+++ src/main/java/com/gatepass/config/SecurityConfig.java
@@ -7,6 +7,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,23 +27,34 @@ public class SecurityConfig {
 
         httpSecurity.authorizeHttpRequests(auth -> auth
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                .antMatchers("/index","/pages/member-request","/staticfrags/footer.html","/staticfrags/header.html").permitAll()
-                .anyRequest().authenticated())  //All the request should be authenticated to avoid the ambiguity in application
-                .formLogin(Customizer.withDefaults());
+                .antMatchers("/index", "/pages/member-request").permitAll()
+                .antMatchers("/staticfrags/footer.html", "/staticfrags/header.html").permitAll()
+                .antMatchers( "/staticfrags/mediafiles/JSCOE_logo.png").permitAll()
+                .anyRequest().authenticated());
+
+        httpSecurity
+                .formLogin(form -> form
+                        .loginPage("/index") // Specify custom login page if any
+                        .defaultSuccessUrl("/index", true) // Redirect on successful login
+                        .permitAll());
 
 
         return httpSecurity.build();
 
     }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); // No password encoding
+    }
 
     //Create your own test user details for testing purpose only
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user = User.builder()
-//                .username("user")
-//                .password("{noop}pass") // {noop} indicates no password encoding
-//                .roles("USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(user);
-//    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}pass") // {noop} indicates no password encoding
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 }
