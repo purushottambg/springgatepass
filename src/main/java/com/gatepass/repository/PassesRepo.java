a101e7778eec669bb0c3c5d65d70acc8aad75f5c
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-02-04 12:39:03 +0530

    DTO parsing issue resolved

--- src/main/java/com/gatepass/repository/PassesRepo.java
+++ src/main/java/com/gatepass/repository/PassesRepo.java
@@ -1,7 +1,10 @@
 package com.gatepass.repository;
 
+import com.gatepass.models.PassEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PassesRepo {
+public interface PassesRepo  extends JpaRepository<PassEntity, Long> {
+
 }
--- src/main/java/com/gatepass/service/PassesService.java
+++ src/main/java/com/gatepass/service/PassesService.java
@@ -1,10 +1,25 @@
 package com.gatepass.service;
 
+import com.gatepass.controllers.OpsController;
 import com.gatepass.dtos.PassDTO;
+import com.gatepass.models.PassEntity;
+import com.gatepass.repository.PassesRepo;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PassesService {
+    private final PassesRepo passesRepo;
+    private final ModelMapper modelMapper;
+    Logger log = LoggerFactory.getLogger(OpsController.class);
     public void savePass(PassDTO passDTO) {
+        log.info("Date format for intime is: {}", passDTO.getIntime());
+        log.info("Date format for outtime is: {}", passDTO.getOuttime());
+        PassEntity tobeSavedPass = modelMapper.map(passDTO, PassEntity.class);
+        passesRepo.save(tobeSavedPass);
     }
 }
