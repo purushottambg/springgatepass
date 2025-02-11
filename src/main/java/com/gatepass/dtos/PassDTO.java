d9c9e231700285d2801a6eb95b84635b319d7d2c
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-02-11 22:55:29 +0530

    Before all and before each used

--- src/main/java/com/gatepass/dtos/PassDTO.java
+++ src/main/java/com/gatepass/dtos/PassDTO.java
@@ -1,6 +1,7 @@
 package com.gatepass.dtos;
 
 import com.gatepass.models.StaffEntity;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
--- src/test/java/com/gatepass/GatepassApplicationTests.java
+++ src/test/java/com/gatepass/GatepassApplicationTests.java
@@ -2,21 +2,33 @@ package com.gatepass;
 
 import com.gatepass.dtos.PassDTO;
 import com.gatepass.service.PassesService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class GatepassApplicationTests {
 	@Autowired
 	PassesService passesService;
-	@Autowired
-	PassDTO passDTO;
+	static Logger logger = LoggerFactory.getLogger(GatepassApplicationTests.class);
+
+	@BeforeAll
+	public static void beforeEach(){
+		logger.info("This is before each annotation!");
+	}
+
+	@Test
+	@DisplayName("Print Greet")
+	public void greetEach(){
+		logger.info("Greet method");
+	}
 	@Test
 	@DisplayName("CheckCreatedPassId")
 	public void checkPassIdOfNewlyGenereatedPass(){
-
-		Long createdPassId = passesService.savePass(passDTO);
-		System.out.println(createdPassId);
+		logger.info("First Test using JUnit :)");
 	}
 
 
