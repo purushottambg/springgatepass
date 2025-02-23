d287ac7545544ff134d8a6882869409fa6712c00
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-02-23 14:18:32 +0530

    Both the tests are succesful

--- src/test/java/com/gatepass/service/JWTServiceTest.java
+++ src/test/java/com/gatepass/service/JWTServiceTest.java
@@ -1,29 +1,30 @@
 package com.gatepass.service;
 
+import com.gatepass.models.MembershipEntity;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootTest
+
 class JWTServiceTest {
 
     @Autowired
     private JWTService jwtService;
 
     @Test
-    void generateToken(){
+    void generateToken_fromUser(){
         final Set<GrantedAuthority> authorities = new HashSet<>(
                 Arrays.asList(
                         new SimpleGrantedAuthority("ROLE_USER"),
@@ -33,6 +34,19 @@ class JWTServiceTest {
 
         User user = new User("purushottam6761", "Uni8113@48", true, true, true, true, authorities);
         String token = jwtService.generateToken(user);
-        System.out.println("Generated Token: "+token);
+        System.out.println("Generated Token From User: "+token);
     }
-}
+
+    @Test
+    void generateToken_fromEntity(){
+        MembershipEntity membership = MembershipEntity.builder()
+                .appid(1L)
+                .fname("Purushottam")
+                .username("Purushottam")
+                .designation("desg")
+                .password("Pass")
+        .build();
+        String token = jwtService.generateToken(membership);
+        System.out.println("Generated Token From Entity: "+token);
+        }
+    }
