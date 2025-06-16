0058acbb66a41c23863109025a7f5bc3e70513bd
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-06-16 23:37:37 +0530

    Pass logging activated

--- src/main/java/com/gatepass/service/PassesService.java
+++ src/main/java/com/gatepass/service/PassesService.java
@@ -15,16 +15,19 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PassesService {
+
     private final PassesRepo passesRepo;
     private final StaffRepo staffRepo;
     private final ModelMapper modelMapper;
 
-    //Logger log = LoggerFactory.getLogger(OpsController.class);
+    Logger log = LoggerFactory.getLogger(PassesService.class);
     public Long savePass(PassDTO passDTO) {
 
         /*
         modelmapper.typeMap is a kinda mapper that defines how mapping should take place
          */
+
+        log.info("staff id in service layer is: {}", passDTO.getStaffid());
 
         modelMapper.typeMap(PassDTO.class, PassEntity.class)
                 .setConverter(context -> {
@@ -45,10 +48,10 @@ public class PassesService {
         PassEntity savedPass = passesRepo.save(tobeSavedPass);
 
         if ( savedPass!=null && savedPass.getPassid()!=null ) {
-            //log.info("Pass saved in the Database and pass id is : {}", savedPass.getPassid());
+            log.info("Pass saved in the Database and pass id is : {}", savedPass.getPassid());
             return savedPass.getPassid();
         }else {
-            //log.info("Pass not saved");
+            log.info("Pass not saved");
             throw new FailedToSavePassException("Pass couldn't saved, Try again");
         }
     }
